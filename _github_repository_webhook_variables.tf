d3bb863a99dd49a4765eaf1a05d4a50e7ccc897a
Simon Weald <glitchcrab-github@simonweald.com>
2022-03-02 23:24:47 +0000

    add manifests to configure repo webhooks

---
+++ _github_repository_webhook_variables.tf
@@ -0,0 +1,38 @@
+variable "webhook_enabled" {
+  type        = number
+  default     = 0
+  description = "Toggles webhook creation on or off."
+}
+
+variable "url" {
+  type        = string
+  description = "The URL of the webhook."
+}
+
+variable "content_type" {
+  type        = string
+  description = "The content type for the payload."
+}
+
+variable "insecure_ssl" {
+  type        = bool
+  default     = false
+  description = "Insecure SSL boolean toggle."
+}
+
+variable "secret" {
+  type        = string
+  sensitive   = true
+  description = "The shared secret for the webhook."
+}
+
+variable "active" {
+  type        = bool
+  default     = true
+  description = "Indicate if the webhook should receive events."
+}
+
+variable "events" {
+  type        = list(string)
+  description = "A list of events which should trigger the webhook"
+}
---
+++ github_repository_webhook.tf
@@ -0,0 +1,16 @@
+resource "github_repository_webhook" "webhook" {
+  count = var.webhook_enabled ? 1 : 0
+
+  repository = var.repo_name
+
+  configuration {
+    url          = var.url
+    content_type = var.content_type
+    insecure_ssl = var.insecure_ssl
+    secret       = var.secret
+  }
+
+  active = var.active
+
+  events = [var.events]
+}
