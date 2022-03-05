3d6394a2e7d06594dc76bff02b85ccb147403eb9
Simon Weald <glitchcrab-github@simonweald.com>
2022-03-05 21:13:20 +0000

    add repo name to collaborators module

--- github_repository_collaborator/_variables.tf
+++ github_repository_collaborator/_variables.tf
@@ -1,3 +1,8 @@
+variable "repo_name" {
+  type        = string
+  description = "The GitHub repository name."
+}
+
 variable "collaborator_name" {
   type        = string
   description = "The user to add to the repository as a collaborator."
