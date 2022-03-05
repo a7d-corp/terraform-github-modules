445145d3493604f356b9469c4dc96f4af6a227b1
Simon Weald <7832341+glitchcrab@users.noreply.github.com>
2022-03-05 21:09:29 +0000

    add repo collaborators module (#10)

---
+++ github_repository_collaborator/_variables.tf
@@ -0,0 +1,9 @@
+variable "collaborator_name" {
+  type        = string
+  description = "The user to add to the repository as a collaborator."
+}
+
+variable "collaborator_permission" {
+  type        = string
+  description = "The permission of the outside collaborator for the repository."
+}
